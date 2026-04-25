d8e65873f755bf63d578beff553ad639775618fe
iRedCode <105721420+iRedCode@users.noreply.github.com>
2026-04-25 15:26:51 +0300

    Update cards.js

--- cards.js
+++ cards.js
@@ -5,12 +5,12 @@ const cards = [
     { id: 2, name: "Твой Персонаж 2", anime: "Твое Аниме", rank: "A", image: "https://iredcode.github.io/telegram-mini-app/card2.png" },
     { id: 3, name: "Твой Персонаж 3", anime: "Твое Аниме", rank: "P", image: "https://iredcode.github.io/telegram-mini-app/card3.png" },
     { id: 4, name: "Твой Персонаж 4", anime: "Твое Аниме", rank: "S", image: "https://iredcode.github.io/telegram-mini-app/card4.png" },
-    { id: 5, name: "Твой Персонаж 4", anime: "Твое Аниме", rank: "S", image: "https://iredcode.github.io/telegram-mini-app/card4.png" },
-    { id: 6, name: "Твой Персонаж 4", anime: "Твое Аниме", rank: "S", image: "https://iredcode.github.io/telegram-mini-app/card4.png" },
-    { id: 7, name: "Твой Персонаж 4", anime: "Твое Аниме", rank: "S", image: "https://iredcode.github.io/telegram-mini-app/card4.jpg" },
-    { id: 8, name: "Твой Персонаж 4", anime: "Твое Аниме", rank: "S", image: "https://iredcode.github.io/telegram-mini-app/card4.jpg" },
-    { id: 9, name: "Твой Персонаж 4", anime: "Твое Аниме", rank: "S", image: "https://iredcode.github.io/telegram-mini-app/card4.jpg" },
-    { id: 10, name: "Твой Персонаж 4", anime: "Твое Аниме", rank: "S", image: "https://iredcode.github.io/telegram-mini-app/card4.jpg" },
-    { id: 11, name: "Твой Персонаж 4", anime: "Твое Аниме", rank: "S", image: "https://iredcode.github.io/telegram-mini-app/card4.jpg" }
+    { id: 5, name: "Твой Персонаж 5", anime: "Твое Аниме", rank: "P", image: "https://iredcode.github.io/telegram-mini-app/card5.png" },
+    { id: 6, name: "Твой Персонаж 6", anime: "Твое Аниме", rank: "A", image: "https://iredcode.github.io/telegram-mini-app/card6.png" },
+    { id: 7, name: "Твой Персонаж 7", anime: "Твое Аниме", rank: "A", image: "https://iredcode.github.io/telegram-mini-app/card7.jpg" },
+    { id: 8, name: "Твой Персонаж 8", anime: "Твое Аниме", rank: "P", image: "https://iredcode.github.io/telegram-mini-app/card8.jpg" },
+    { id: 9, name: "Твой Персонаж 9", anime: "Твое Аниме", rank: "S", image: "https://iredcode.github.io/telegram-mini-app/card9.jpg" },
+    { id: 10, name: "Твой Персонаж 10", anime: "Твое Аниме", rank: "P", image: "https://iredcode.github.io/telegram-mini-app/card10.jpg" },
+    { id: 11, name: "Твой Персонаж 11", anime: "Твое Аниме", rank: "A", image: "https://iredcode.github.io/telegram-mini-app/card11.jpg" }
 ];
 module.exports = cards;
