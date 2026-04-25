6893b2b4af2a81138d30454ae3adc41e7b55e176
iRedCode <105721420+iRedCode@users.noreply.github.com>
2026-04-25 14:59:20 +0300

    Update cards.js

--- cards.js
+++ cards.js
@@ -4,6 +4,6 @@ const cards = [
     { id: 1, name: "Твой Персонаж 1", anime: "Твое Аниме", rank: "S", image: "https://iredcode.github.io/telegram-mini-app/card1.jpg" },
     { id: 2, name: "Твой Персонаж 2", anime: "Твое Аниме", rank: "A", image: "https://iredcode.github.io/telegram-mini-app/card2.jpg" },
     { id: 3, name: "Твой Персонаж 3", anime: "Твое Аниме", rank: "P", image: "https://iredcode.github.io/telegram-mini-app/card3.jpg" },
-    { id: 4, name: "Твой Персонаж 4", anime: "Твое Аниме", rank: "P", image: "https://iredcode.github.io/telegram-mini-app/card4.jpg" }
+    { id: 4, name: "Твой Персонаж 4", anime: "Твое Аниме", rank: "S", image: "https://iredcode.github.io/telegram-mini-app/card4.jpg" }
 ];
 module.exports = cards;
