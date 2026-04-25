03c3fecd4f0493dfd89ba703ed0a7e94368f701e
iRedCode <105721420+iRedCode@users.noreply.github.com>
2026-04-25 15:08:23 +0300

    Update cards.js

--- cards.js
+++ cards.js
@@ -1,9 +1,9 @@
 // cards.js
 const cards = [
     // Замени ссылки на свои картинки с GitHub или любого другого хостинга
-    { id: 1, name: "Твой Персонаж 1", anime: "Твое Аниме", rank: "S", image: "https://iredcode.github.io/telegram-mini-app/card1.jpg" },
-    { id: 2, name: "Твой Персонаж 2", anime: "Твое Аниме", rank: "A", image: "https://iredcode.github.io/telegram-mini-app/card2.jpg" },
-    { id: 3, name: "Твой Персонаж 3", anime: "Твое Аниме", rank: "P", image: "https://iredcode.github.io/telegram-mini-app/card3.jpg" },
-    { id: 4, name: "Твой Персонаж 4", anime: "Твое Аниме", rank: "S", image: "https://iredcode.github.io/telegram-mini-app/card4.jpg" }
+    { id: 1, name: "Твой Персонаж 1", anime: "Твое Аниме", rank: "S", image: "https://iredcode.github.io/telegram-mini-app/card1.png" },
+    { id: 2, name: "Твой Персонаж 2", anime: "Твое Аниме", rank: "A", image: "https://iredcode.github.io/telegram-mini-app/card2.png" },
+    { id: 3, name: "Твой Персонаж 3", anime: "Твое Аниме", rank: "P", image: "https://iredcode.github.io/telegram-mini-app/card3.png" },
+    { id: 4, name: "Твой Персонаж 4", anime: "Твое Аниме", rank: "S", image: "https://iredcode.github.io/telegram-mini-app/card4.png" }
 ];
 module.exports = cards;
